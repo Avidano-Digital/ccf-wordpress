9effa7c18e999b291f3bd526461c838b21bce054
Carl Avidano <cavidano@gmail.com>
2018-10-15 04:50:50 -0400

    fixed phone buttons

--- wp-content/themes/ccf-twentyeighteen/include/mobile-menu.php
+++ wp-content/themes/ccf-twentyeighteen/include/mobile-menu.php
@@ -1,15 +1,3 @@
-<div id="search-site" class="bg-light">
-	<form class="container narrow py-4" aria-label="Search" role="search">
-		<div class="form-group mb-0">
-			<input class="form-control form-control-lg shadow" placeholder="Search cheetah.org">
-				<button type="button" class="btn btn-lg btn-white">
-					<span class="fas fa-search"></span>
-					<span class="sr-only">Search</span>
-				</button>
-			</div>
-	</form>
-</div>
-
 <nav class="py-4">
     
     <div id="accordion" class="accordion-group" role="tablist">
