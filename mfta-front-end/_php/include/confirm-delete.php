145a58e72ddb587cacbd4baa4433b3f6797b7b32
Carl Avidano <cavidano@gmail.com>
2018-11-21 01:38:02 -0500

    updated after reverting

--- mfta-front-end/_php/include/confirm-delete.php
+++
@@ -1,8 +0,0 @@
-<!-- Alert Delete Confirmation -->
-<div class="confirm-alert-close" tab-index="-1" hidden>
-    <p>Delete Permanently?</p>
-    <div class="button-row">
-        <div><button class="confirm" type="button">Yes</button></div>
-        <div><button class="cancel">No</button></div>
-    </div>              
-</div>
